63b10bb4ee6985090fc3dff23492a79c8e08e9c4
Nukelar <meepness@outlook.com>
2017-01-16 00:38:15 +0000

    Added Tons

---
+++ I Like Trains.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	cont << "I \nLike \nTrains \nAnd \nEverything \nIs \nAwesome";
+	return 0;
+}
---
+++ Welcome To My Special Hell.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "I\n like\n trains\n";
+	cout << "Welcome\n to\n my \nspecial \nhell!\n";
+}
