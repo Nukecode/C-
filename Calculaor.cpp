9c3eee3cd5e418fc37a2831e5a85077646205cce
Nukelar <meepness@outlook.com>
2017-01-16 20:13:52 +0000

    Added Crash.cpp
    DO NOT RUN THIS

--- Calculaor.cpp
+++ Calculaor.cpp
@@ -3,26 +3,27 @@ using namespace std;
 int main()
 {
  	int exit;
- 	cout << "Calculator" << endl;      
-	cout << "List Of Functions" << endl;
-	cout << "Add = 1" << endl;
-	cout << "Subtract = 2" << endl;
-	cout << "Divide = 3" << endl;
-	cout << "Multiply = 4" << endl;
+ 	cout << "Calculator\n" << endl;      
+	cout << "List Of Functions\n" << endl;
+	cout << "1. Add" << endl;
+	cout << "2. Subtract" << endl;
+	cout << "3. Divide" << endl;
+	cout << "4. Multiply" << endl;
 	cout << endl << endl ;"Type The Function You Would like to run";
-	int choice;
-	cin choice;
-	if (choice == "1") {
+	int func;
+	cin >> func;
+	if (func == 1) {
 		cout<< "Input First Number" << endl;
 		int x; 
-		cin x;
+		cin >> x;
 		cout << "Input Second Number" << endl;
 		int y;
-		cin y;
-		sum = x+y;
-		cout << "Sum =" sum << endl;
+		cin >> y;
+	
+		int sum = x+y;
+		cout << "Sum =" << sum << endl;
 		cout << "Press any key then enter too terminate"
-		cin exit;
+		
 	}
 	
 }
---
+++ Crash.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x;
+	while(x != -1){
+		x ++;
+		cout << x;
+	}
+}
