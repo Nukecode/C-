cc6e90ec9267cbdb4fb01212b2064bae5fe6a94a
Nukelar <meepness@outlook.com>
2017-01-16 00:54:41 +0000

    Added Program Too Add Numbers

---
+++ Adding.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Ask To Input First Number
+	cout << "Please Input The First Number\n";
+	//Input Number
+	int num;
+	cin >> num;
+	//Ask To Input 2nd Number
+	cout<< "Please Input The Second Number\n";
+	//Imput Number
+	int num2;
+	cin >> num2;
+	int ans; //Declare ans as a varible
+	ans = num2 + num; //Set ans value to num + num2
+	cout << "Answer Is, "<< ans; //Display a answer
+	return 0;
+}
