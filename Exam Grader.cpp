a43b1ca04ecb5bfc34c882d3e1002ace707bab1f
Nukelar <meepness@outlook.com>
2017-01-16 19:47:55 +0000

    Finished Exam Grader.cpp

--- Exam Grader.cpp
+++ Exam Grader.cpp
@@ -9,26 +9,86 @@ int main() {
 	cin >> mark;
 	char grade;
 	char fail_pass;
-	if (mark <= 50) {
+	if (mark <= 60) {
 		char fail_pass[] = "Failed"; 
-		if (mark >= 40) {
-		    char grade[] = "E";
-			cout << "You ";
-			cout << fail_pass;
-			cout << " And Got an ";
-			cout << grade:
-		}
+		if (mark >= 60) {
+		    if(mark <= 50) {
+		    	char grade[] = "D";
+				cout << "You ";
+				cout << fail_pass;
+				cout << " And Got an ";
+				cout << grade;
+			}
+			else{
+				if(mark <= 40) {
+					char grade[] = "E";
+					cout << "You ";
+					cout << fail_pass;
+					cout << " And Got an ";
+					cout << grade;
+
+				}
+				else{
+					char grade[] = "U";
+					cout << "You ";
+					cout << fail_pass;
+					cout << " And Got an ";
+					cout << grade;
+
+				}
+			}
+		
+		
+	}
+	else{
+		char pass_fail[] = "Passed";
+		if(mark <= 95){
+			if(mark <= 90){
+				if (mark <= 80){
+					if (mark <= 70){
+						if (mark <= 65){
+							char grade[] = "C";
+							cout << "You ";
+							cout << fail_pass;
+							cout << " And Got an ";
+							cout << grade;
+						}
+					}
+					else{
+						char grade[] = "B";
+						cout << "You ";
+						cout << fail_pass;
+						cout << " And Got an ";
+						cout << grade;
+
+					}
+				}
+				else{
+					char grade[] = "A";
+					cout << "You ";
+					cout << fail_pass;
+					cout << " And Got an ";
+					cout << grade;
+
+				}
+			}
+			else{
+				char grade[] = "A*";
+				cout << "You ";
+				cout << fail_pass;
+				cout << " And Got an ";
+				cout << grade;
+
+			}
+		} 
 		else{
-			char grade[] = "U";
+			char grade[] = "A**";
 			cout << "You ";
 			cout << fail_pass;
 			cout << " And Got an ";
 			cout << grade;
+
 		}
-		
 	}
-	else{
-		char pass_fail
-		if(mark <=) 
-	}
+}
 }
