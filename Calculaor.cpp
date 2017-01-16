1fc23992c6cd79e9cb9f7335a1e5e4da3bed0a1d
Nukelar <meepness@outlook.com>
2017-01-16 18:13:41 +0000

    Updated Calc (Still Not Finished

--- Calculaor.cpp
+++ Calculaor.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 int main()
 {
- 	
- 	cout << "Calculator/n";      
-	cout << "List Of Functions/n";
-	cout << "Add,/n Subtract,/n Div,/n Multi/n";
+ 	int exit;
+ 	cout << "Calculator" << endl;      
+	cout << "List Of Functions" << endl;
+	cout << "Add = 1" << endl;
+	cout << "Subtract = 2" << endl;
+	cout << "Divide = 3" << endl;
+	cout << "Multiply = 4" << endl;
+	cout << endl << endl ;"Type The Function You Would like to run";
+	int choice;
+	cin choice;
+	if (choice = "1") {
+		cout<< "Input First Number" << endl;
+		int x; 
+		cin x;
+		cout << "Input Second Number" << endl;
+		int y;
+		cin y;
+		sum = x+y;
+		cout << "Sum =" sum << endl;
+		cout << "Press any key then enter too terminate"
+		cin exit;
+	}
+	
 }
