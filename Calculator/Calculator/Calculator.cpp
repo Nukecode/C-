9efbb22de020141debd752084dfa3ec827568fb5
Nukelar <meepness@outlook.com>
2017-01-17 07:55:01 +0000

    Huge Commit 3

---
+++ Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,16 @@
+// Calculator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	cout << "Calculator\n\n\n" ;
+	cout << "Type 'Functions' For a list of avalible functions\n\n ";
+    return 0;
+}
+
