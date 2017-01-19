0e41e7cd4845431e68e9270b99ccfb5822d30592
Nukelar <meepness@outlook.com>
2017-01-19 12:34:22 +0000

    Oh Yay More Commits

--- Calc2/Calc2/Calc2.cpp
+++ Calc2/Calc2/Calc2.cpp
@@ -2,10 +2,16 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
+	cout << "             Calculator.               " << endl;
+	cout << "List Of Functions/n/n";
+	cout << "1. Add/n2. Subtract/n3. Multiply4. Divide";
+
     return 0;
 }
 
